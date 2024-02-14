56e948ee0e6d78f5fd909ddf955c20dc92f86fbf
Nathan Lazzaro <rplazzaro@hotmail.com>
2024-02-14 20:24:54 -0300

    3commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,18 +4,23 @@ import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main
+{
     public static void main(String[] args)
     {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
        Scanner scanner = new Scanner(System.in);
-       String rua, numero;
-       System.out.println("Favor digitar o nome da rua:");
-       rua = scanner.next();
-       System.out.println("Favor digitar o numero da rua:");
-       numero = scanner.next();
-       System.out.println(rua + "," + numero);
-
+       String rua = "", numero;
+        while( !rua.equals("exit") )
+        {
+            System.out.println("Favor digitar o nome da rua:");
+            rua = scanner.next();
+            if(rua.equals("exit"))
+                System.exit(0);
+            System.out.println("Favor digitar o numero da rua:");
+            numero = scanner.next();
+            System.out.println(rua + "," + numero);
+        }
     }
 }
